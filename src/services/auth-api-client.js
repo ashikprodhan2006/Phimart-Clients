792f93eb49889ee47418da476ad001e4ac770b97
Md Ashik Prodhan <ashikprodhan2006@gmail.com>
2026-07-28 15:40:39 +0600

    rady for deployment

--- src/services/auth-api-client.js
+++ src/services/auth-api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-    baseURL: "https://phi-mart-kohl.vercel.app/api/v1",
+    baseURL: "https://phi-mart-five.vercel.app/api/v1",
 });
 
 export default authApiClient;
